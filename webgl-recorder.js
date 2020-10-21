bef0e65596e981ee382126587e2dcbe0fc7748e2
Jonathan Chan <jyc@eqv.io>
2020-10-20 23:48:54 -0700

    Misc. updates, upstreaming Figma version (#2)
    * Replace var with const/let.
    
    * Also create variablers for WebGLVertexArrayObjects.
    
    * Also save results with with `typeof result == 'object'`.
    
    Sometimes browsers return things like this.
    
    * Create fake context using defineProperty so that getters can be live.
    
    The previous approach of just copying values caused problems with Figma,
    which sets gl.canvas.width and then reads it back.
    
    * Use map instead of looping and pushing on arguments.
    
    * Use string interpolation syntax.
    
    * Also log for which function we didn't support an argument type.
    
    * Use consistent if/else spacing.

--- webgl-recorder.js
+++ webgl-recorder.js
@@ -13,29 +13,24 @@
   };
 
   HTMLCanvasElement.prototype.getContext = function(type) {
-    var canvas = this;
-    var context = getContext.apply(canvas, arguments);
+    const canvas = this;
+    const context = getContext.apply(canvas, arguments);
 
     if (type === 'webgl' || type === 'experimental-webgl') {
-      var oldWidth = canvas.width;
-      var oldHeight = canvas.height;
-      var oldFrameCount = frameSincePageLoad;
-      var trace = [];
-      var variables = {};
-      var fakeContext = {
-        trace: trace,
-        compileTrace: compileTrace,
-        downloadTrace: downloadTrace,
-      };
+      let oldWidth = canvas.width;
+      let oldHeight = canvas.height;
+      let oldFrameCount = frameSincePageLoad;
+      const trace = [];
+      const variables = {};
 
       trace.push('  gl.canvas.width = ' + oldWidth + ';');
       trace.push('  gl.canvas.height = ' + oldHeight + ';');
 
       function compileTrace() {
-        var text = 'function* render(gl) {\n';
+        let text = 'function* render(gl) {\n';
         text += '  // Recorded using https://github.com/evanw/webgl-recorder\n';
-        for (var key in variables) {
-          text += '  var ' + key + 's = [];\n';
+        for (let key in variables) {
+          text += '  const ' + key + 's = [];\n';
         }
         text += trace.join('\n');
         text += '\n}\n';
@@ -43,8 +38,8 @@
       }
 
       function downloadTrace() {
-        var text = compileTrace();
-        var link = document.createElement('a');
+        const text = compileTrace();
+        const link = document.createElement('a');
         link.href = URL.createObjectURL(new Blob([text], {type: 'application/javascript'}));
         link.download = 'trace.js';
         document.body.appendChild(link);
@@ -61,10 +56,14 @@
             value instanceof WebGLShader ||
             value instanceof WebGLShaderPrecisionFormat ||
             value instanceof WebGLTexture ||
-            value instanceof WebGLUniformLocation) {
-          var name = value.constructor.name;
-          var list = variables[name] || (variables[name] = []);
-          var index = list.indexOf(value);
+            value instanceof WebGLUniformLocation ||
+            value instanceof WebGLVertexArrayObject ||
+            // In Chrome, value won't be an instanceof WebGLVertexArrayObject.
+            (value && value.constructor.name == "WebGLVertexArrayObjectOES") ||
+            typeof value === 'object') {
+          const name = value.constructor.name;
+          const list = variables[name] || (variables[name] = []);
+          let index = list.indexOf(value);
 
           if (index === -1) {
             index = list.length;
@@ -77,14 +76,13 @@
         return null;
       }
 
-      for (var key in context) {
-        var value = context[key];
-
-        if (typeof value === 'function') {
-          fakeContext[key] = function(key, value) {
-            return function() {
-              var result = value.apply(context, arguments);
-              var args = [];
+      function patch(name, object) {
+        const patched = {};
+        for (const key in object) {
+          const value = object[key];
+          if (typeof value === 'function') {
+            patched[key] = function () {
+              const result = value.apply(object, arguments);
 
               if (frameSincePageLoad !== oldFrameCount) {
                 oldFrameCount = frameSincePageLoad;
@@ -98,45 +96,55 @@
                 trace.push('  gl.canvas.height = ' + oldHeight + ';');
               }
 
-              for (var i = 0; i < arguments.length; i++) {
-                var arg = arguments[i];
-
+              const args = Array.prototype.map.call(arguments, arg => {
                 if (typeof arg === 'number' || typeof arg === 'boolean' || typeof arg === 'string' || arg === null) {
-                  args.push(JSON.stringify(arg));
-                }
-
-                else if (ArrayBuffer.isView(arg)) {
-                  args.push('new ' + arg.constructor.name + '([' + Array.prototype.slice.call(arg) + '])');
-                }
-
-                else {
-                  var variable = getVariable(arg);
+                  return JSON.stringify(arg);
+                } else if (ArrayBuffer.isView(arg)) {
+                  return `new ${arg.constructor.name}([${Array.prototype.slice.call(arg)}])`;
+                } else {
+                  const variable = getVariable(arg);
                   if (variable !== null) {
-                    args.push(variable);
-                  }
-
-                  else {
-                    console.warn('unsupported value:', arg);
-                    args.push('null');
+                    return variable;
+                  } else {
+                    console.warn('unsupported value:', arg, `in call to ${name}.${key}`);
+                    return 'null';
                   }
                 }
-              }
+              });
 
-              var text = 'gl.' + key + '(' + args.join(', ') + ');';
-              var variable = getVariable(result);
-              if (variable !== null) text = variable + ' = ' + text;
+              let text = `${name}.${key}(${args.join(', ')});`;
+              const variable = getVariable(result);
+              if (variable !== null) text = `${variable} = ${text}`;
               trace.push('  ' + text);
 
+              if (result === null) return null;
+              if (result === undefined) return undefined;
+              // In Firefox, getExtension returns things with constructor.name == 'Object', but in
+              // Chrome getExtension returns unique constructor.names.
+              if (result.constructor.name === 'Object' || key == 'getExtension') {
+                return patch(variable, result);
+              }
               return result;
             };
-          }(key, value);
+          } else { // typeof value !== function
+            Object.defineProperty(patched, key, {
+              configurable: false,
+              enumerable: true,
+              get() {
+                return object[key];
+              }
+            });
+          }
         }
-
-        else {
-          fakeContext[key] = value;
-        }
+        return patched;
       }
 
+      const fakeContext = patch('gl', context);
+      Object.assign(fakeContext, {
+        trace: trace,
+        compileTrace: compileTrace,
+        downloadTrace: downloadTrace,
+      });
       return fakeContext;
     }
 
